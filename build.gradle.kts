0c27b988056f7b84684e07f6bceac05b20cb7719
Modificator <yunshangcn@gmail.com>
2020-10-25 14:18:59 +0800

    upgrade agp version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.0-alpha13")
+        classpath("com.android.tools.build:gradle:4.2.0-alpha14")
         classpath(kotlin("gradle-plugin", version = kotlinVersion))
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
